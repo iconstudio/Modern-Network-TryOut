268c3042b7f7a51c95e9a23361c16a83eb817a47
Iconer <yoyofa2@hotmail.com>
2023-10-09 15:20:42 +0900

    Define some methods

--- Modern Network/src/Socket.cpp
+++ Modern Network/src/Socket.cpp
@@ -1,8 +1,32 @@
+module;
 module Net.Socket;
 
 using namespace net;
 
-SocketSendingResult Socket::Send(std::span<const std::byte> buffer) const noexcept
+SocketSendingResult
+Socket::Send(std::span<const std::byte> buffer)
+const noexcept
 {
 	return SocketSendingResult(0);
+}
+
+SocketSendingResult
+Socket::Send(const std::byte* const& buffer, size_t size)
+const noexcept
+{
+	return SocketSendingResult();
+}
+
+SocketReceivingResult
+Socket::Receive(std::span<std::byte> buffer)
+const noexcept
+{
+	return SocketReceivingResult();
+}
+
+SocketReceivingResult
+Socket::Receive(const std::byte* const& buffer, size_t size)
+const noexcept
+{
+	return SocketReceivingResult();
 }
