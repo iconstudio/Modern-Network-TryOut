c6e2a5e8ad0edbafd03799b65c2b6f7afdb62646
Iconer <yoyofa2@hotmail.com>
2023-10-11 21:07:57 +0900

    Test msg

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,8 +5,15 @@ import Net.Application;
 
 int main()
 {
+	std::println("Init");
 	net::Application app{};
+
+	std::println("Awake");
 	app.Awake();
+
+	std::println("Start");
 	app.Start();
+
+	std::println("Update");
 	app.Update(0);
 }
