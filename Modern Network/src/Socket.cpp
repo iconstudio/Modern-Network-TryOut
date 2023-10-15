b4e2dd676e5972db9651095b6b22945253d81948
Iconer <yoyofa2@hotmail.com>
2023-10-16 08:11:07 +0900

    Clear includes

--- Modern Network/src/Socket.cpp
+++ Modern Network/src/Socket.cpp
@@ -1,13 +1,9 @@
 module;
 #pragma comment(lib, "Ws2_32.lib")
 #include <WinSock2.h>
-#include <ws2ipdef.h>
-#include <WS2tcpip.h>
-#include <MSWSock.h>
 
 module Net.Socket;
 import <type_traits>;
-import <functional>;
 
 using namespace net;
 
