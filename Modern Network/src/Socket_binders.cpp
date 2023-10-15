b88cafcfacef48bf8796f2cb50bb5accbe8b47cb
Iconer <yoyofa2@hotmail.com>
2023-10-16 06:28:15 +0900

    5 source code files for `Socket` are added

---
+++ Modern Network/src/Socket_binders.cpp
@@ -0,0 +1,8 @@
+module;
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <ws2ipdef.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+module Net.Socket;
+
---
+++ Modern Network/src/Socket_senders.cpp
@@ -0,0 +1,5 @@
+module;
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+module Net.Socket;
+
