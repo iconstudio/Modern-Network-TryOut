551228c279b7c370300a66e0a22fbfaebd8af2a9
Iconer <yoyofa2@hotmail.com>
2023-10-16 06:29:29 +0900

    Senders of `Socket`

--- Modern Network/src/Socket.cpp
+++ Modern Network/src/Socket.cpp
@@ -16,56 +16,6 @@ using namespace net;
 
 const Socket::EmptySocketType Socket::EmptySocket = {};
 
-SocketSendingResult
-Socket::Send(std::span<const std::byte> memory)
-const noexcept
-{
-	WSABUF buffer
-	{
-		.len = static_cast<ULONG>(memory.size_bytes()),
-		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory.data())),
-	};
-
-	DWORD bytes = 0;
-	int result = WSASend(myHandle, std::addressof(buffer), 1, std::addressof(bytes), 0, nullptr, nullptr);
-	if (0 == result)
-	{
-		return bytes;
-	}
-	else
-	{
-		if (auto error = AcquireSendingError(); error != SendingErrorCodes::PendedIoOperation)
-		{
-			return unexpected(std::move(error));
-		}
-		else
-		{
-			return 0U;
-		}
-	}
-}
-
-SocketSendingResult
-Socket::Send(const std::byte* const& memory, size_t size)
-const noexcept
-{
-	return SocketSendingResult();
-}
-
-SocketReceivingResult
-Socket::Receive(std::span<std::byte> memory)
-const noexcept
-{
-	return SocketReceivingResult();
-}
-
-SocketReceivingResult
-Socket::Receive(const std::byte* const& memory, size_t size)
-const noexcept
-{
-	return SocketReceivingResult();
-}
-
 Socket::Socket(EmptySocketType)
 noexcept
 	: Socket(INVALID_SOCKET, InternetProtocols::Unknown, IpAddressFamily::Unknown)
@@ -83,6 +33,20 @@ noexcept
 Socket::~Socket()
 noexcept
 {}
+
+SocketReceivingResult
+Socket::Receive(std::span<std::byte> memory)
+const noexcept
+{
+	return SocketReceivingResult();
+}
+
+SocketReceivingResult
+Socket::Receive(const std::byte* const& memory, size_t size)
+const noexcept
+{
+	return SocketReceivingResult();
+}
 
 SocketResult
 Socket::Bind(const IpAddress& address, const std::uint16_t& port)
--- Modern Network/src/Socket_senders.cpp
+++ Modern Network/src/Socket_senders.cpp
@@ -3,3 +3,38 @@ module;
 #include <WinSock2.h>
 module Net.Socket;
 
+net::SocketSendingResult
+net::Socket::Send(std::span<const std::byte> memory)
+const noexcept
+{
+	WSABUF buffer
+	{
+		.len = static_cast<ULONG>(memory.size_bytes()),
+		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory.data())),
+	};
+
+	DWORD bytes = 0;
+	int result = WSASend(myHandle, std::addressof(buffer), 1, std::addressof(bytes), 0, nullptr, nullptr);
+	if (0 == result)
+	{
+		return bytes;
+	}
+	else
+	{
+		if (auto error = AcquireSendingError(); error != SendingErrorCodes::PendedIoOperation)
+		{
+			return unexpected(std::move(error));
+		}
+		else
+		{
+			return 0U;
+		}
+	}
+}
+
+net::SocketSendingResult
+net::Socket::Send(const std::byte* const& memory, size_t size)
+const noexcept
+{
+	return SocketSendingResult();
+}
