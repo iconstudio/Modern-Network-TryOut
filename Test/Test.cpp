1010468af36e47c0128f7943c99faa075afee568
Iconer <yoyofa2@hotmail.com>
2023-10-16 05:46:08 +0900

    Testing multiple sockets

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,6 +2,9 @@
 
 import <print>;
 import Net.Application;
+import Net.Socket;
+import Net.IpAddress;
+import Net.IpAddress.IPv4;
 
 int main()
 {
@@ -14,6 +17,25 @@ int main()
 	std::println("Start");
 	app.Start();
 
+	net::Socket test_socket1 = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
+
+	auto binded = test_socket1.Bind(net::IPv4Address::Loopback, 52001);
+	if (binded.has_value())
+	{
+		std::println("binded!");
+	}
+
+	net::Socket test_socket2 = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
+
+	binded = test_socket2.Bind(net::IPv4Address::Loopback, 52002);
+	if (binded.has_value())
+	{
+		std::println("binded!");
+	}
+
 	std::println("Update");
-	app.Update(0);
+	while (true)
+	{
+		app.Update(0);
+	}
 }
