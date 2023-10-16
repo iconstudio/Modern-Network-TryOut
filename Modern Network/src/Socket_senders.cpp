384e07857ca8812533c611ca9130de30cbdb5fa5
Iconer <yoyofa2@hotmail.com>
2023-10-16 10:06:55 +0900

    `Send` method

--- Modern Network/src/Socket_senders.cpp
+++ Modern Network/src/Socket_senders.cpp
@@ -14,8 +14,11 @@ const noexcept
 	};
 
 	DWORD bytes = 0;
-	int result = WSASend(myHandle, std::addressof(buffer), 1, std::addressof(bytes), 0, nullptr, nullptr);
-	if (0 == result)
+	if (0 == WSASend(myHandle
+		, std::addressof(buffer), 1
+		, std::addressof(bytes)
+		, 0
+		, nullptr, nullptr))
 	{
 		return bytes;
 	}
@@ -36,5 +39,30 @@ net::SocketSendingResult
 net::Socket::Send(const std::byte* const& memory, size_t size)
 const noexcept
 {
-	return SocketSendingResult();
+	WSABUF buffer
+	{
+		.len = static_cast<ULONG>(size),
+		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory)),
+	};
+
+	DWORD bytes = 0;
+	if (0 == WSASend(myHandle
+		, std::addressof(buffer), 1
+		, std::addressof(bytes)
+		, 0
+		, nullptr, nullptr))
+	{
+		return bytes;
+	}
+	else
+	{
+		if (auto error = AcquireSendingError(); error != SendingErrorCodes::PendedIoOperation)
+		{
+			return unexpected(std::move(error));
+		}
+		else
+		{
+			return 0U;
+		}
+	}
 }
