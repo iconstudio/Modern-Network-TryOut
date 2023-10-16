1aebc1ff68029b6ea7e465f2666284b11e6338c1
Iconer <yoyofa2@hotmail.com>
2023-10-16 10:10:28 +0900

    Plain `Receive` methods

--- Modern Network/src/Socket_receivers.cpp
+++ Modern Network/src/Socket_receivers.cpp
@@ -7,12 +7,64 @@ net::SocketReceivingResult
 net::Socket::Receive(std::span<std::byte> memory)
 const noexcept
 {
-	return SocketReceivingResult();
+	WSABUF buffer
+	{
+		.len = static_cast<ULONG>(memory.size_bytes()),
+		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory.data())),
+	};
+
+	DWORD bytes = 0;
+	DWORD flags = 0;
+	if (0 == WSARecv(myHandle
+		, std::addressof(buffer), 1
+		, std::addressof(bytes)
+		, std::addressof(flags)
+		, nullptr, nullptr))
+	{
+		return bytes;
+	}
+	else
+	{
+		if (auto error = AcquireReceivingError(); error != SendingErrorCodes::PendedIoOperation)
+		{
+			return unexpected(std::move(error));
+		}
+		else
+		{
+			return 0U;
+		}
+	}
 }
 
 net::SocketReceivingResult
 net::Socket::Receive(const std::byte* const& memory, size_t size)
 const noexcept
 {
-	return SocketReceivingResult();
+	WSABUF buffer
+	{
+		.len = static_cast<ULONG>(size),
+		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory)),
+	};
+
+	DWORD bytes = 0;
+	DWORD flags = 0;
+	if (0 == WSARecv(myHandle
+		, std::addressof(buffer), 1
+		, std::addressof(bytes)
+		, std::addressof(flags)
+		, nullptr, nullptr))
+	{
+		return bytes;
+	}
+	else
+	{
+		if (auto error = AcquireReceivingError(); error != SendingErrorCodes::PendedIoOperation)
+		{
+			return unexpected(std::move(error));
+		}
+		else
+		{
+			return 0U;
+		}
+	}
 }
