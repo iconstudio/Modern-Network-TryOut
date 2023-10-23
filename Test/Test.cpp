0f77774eae1c676fbf3c68b94e27948beabbdc62
Iconer <yoyofa2@hotmail.com>
2023-10-23 17:21:44 +0900

    Test echo server

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,81 +1,69 @@
 ﻿#pragma comment(lib, "Modern Network.lib")
 #include <print>
+#include <span>
 
-import Net.Constraints;
-import Net.Application;
+import Net;
 import Net.IpAddress;
 import Net.IpAddress.IPv4;
 import Net.Socket;
-import Net.Task;
 import Net.Coroutine;
 import Net.Coroutine.Awaiter.Timed;
+import Net.Coroutine.Awaiter.Concurrent;
 
-net::Coroutine Worker2()
+net::Socket listener = net::Socket::EmptySocket;
+
+net::Coroutine Worker()
 {
-	net::Socket test_socket2 = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
+	co_await net::coroutine::WaitForSeconds(1);
 
-	auto binded = test_socket2.Bind(net::IPv4Address::Loopback, 52002);
-	if (binded.has_value())
+	net::IoContext listen_context{};
+	std::byte buffer[512]{};
+	size_t recv_size = 0;
+
+	while (true)
 	{
-		std::println("binded! (2)");
-	}
-
-	auto a = co_await net::coroutine::WaitForSeconds(3);
-
-	//auto connector = test_socket2.ConnectAsync(net::IPv4Address::Loopback, 52000);
-	//auto b = co_await connector;
-
-	auto sdsc = co_await test_socket2.ConnectAsync(net::IPv4Address::Loopback, 52000);
-
-	//if (b.has_value())
-	if (sdsc.has_value())
-	{
-		std::println("connected! (2)");
+		auto recv = co_await listener.ReceiveAsync(&listen_context, std::span{ buffer });
+		if (recv.has_value())
+		{
+			recv_size = recv.value();
+		}
+		else
+		{
+			break;
+		}
+		auto sent = co_await listener.SendAsync(&listen_context, buffer, recv_size);
 	}
 
 	co_return;
-}
-
-void Worker()
-{
-	net::Socket test_socket1 = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
-
-	auto binded = test_socket1.Bind(net::IPv4Address::Loopback, 52001);
-	if (binded.has_value())
-	{
-		std::println("binded! (1)");
-	}
-
-	auto connected = test_socket1.Connect(net::IPv4Address::Loopback, 52000);
-	if (connected.has_value())
-	{
-		std::println("connected! (1)");
-	}
-
-	Worker2();
-
-	std::println("Worker done!");
 }
 
 int main()
 {
 	std::println("=========$ Server $=========");
 	std::println("=========== Init ===========");
-	net::Application app{};
+	net::core::Initialize();
 
 	std::println("=========== Awake ===========");
-	app.Awake();
+
+	listener = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
+
+	if (listener.Bind(net::IPv4Address::Loopback, 52000).has_value())
+	{
+		std::println("The listener is binded!");
+	}
+
+	listener.IsAddressReusable = true;
 
 	std::println("=========== Start ===========");
-	app.Start();
 
-	Worker();
+	if (listener.Open().has_value())
+	{
+		std::println("The listener is opened!");
+	}
 
 	std::println("=========== Update ===========");
 
-	char buffer[512]{};
-	while (true)
-	{
-		app.Update(0);
-	}
+	Worker();
+
+	net::core::Annihilate();
 }
