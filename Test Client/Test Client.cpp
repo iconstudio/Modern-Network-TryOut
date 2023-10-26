29bdb0e778bff9853f90e691adc490cb67637595
Iconer <yoyofa2@hotmail.com>
2023-10-26 18:59:44 +0900

    Testing clients

--- Test Client/Test Client.cpp
+++ Test Client/Test Client.cpp
@@ -1,31 +1,54 @@
 ﻿#pragma comment(lib, "Modern Network.lib")
 #include <print>
 
-import Net.Constraints;
-import Net.Application;
+import Net;
 import Net.IpAddress;
 import Net.IpAddress.IPv4;
 import Net.Socket;
-import Net.Task;
 import Net.Coroutine;
+import Net.Coroutine.Awaiter.Timed;
+
+net::Socket client = net::Socket::EmptySocket;
+
+net::Coroutine Worker()
+{
+	co_await net::coroutine::WaitForSeconds(1);
+
+	net::EndPoint server{ net::IPv4Address::Loopback, 52000 };
+
+	if (auto connectum = co_await client.ConnectAsync(server); connectum.has_value())
+	{
+		std::println("The client is connected!");
+	}
+	else
+	{
+		std::println("The client cannot be connected!");
+	}
+
+	char buffer[512]{};
+	while (true)
+	{
+	}
+}
 
 int main()
 {
 	std::println("=========$ Client $=========");
 	std::println("=========== Init ===========");
-	net::Application app{};
+	net::core::Initialize();
 
 	std::println("=========== Awake ===========");
-	app.Awake();
+	client = net::Socket::Create(net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
 
 	std::println("=========== Start ===========");
-	app.Start();
+
+	if (client.Bind(net::IPv4Address::Loopback, 52001).has_value())
+	{
+		std::println("The client is binded!");
+	}
 
 	std::println("=========== Update ===========");
+	Worker();
 
-	char buffer[512]{};
-	while (true)
-	{
-		app.Update(0);
-	}
+	net::core::Annihilate();
 }
