de3491d4b104bfd8eac79c5bfb5dc8f9414597ad
Iconer <yoyofa2@hotmail.com>
2023-11-01 00:22:50 +0900

    Formatting `IpAddressFamily`

--- Modern Network/src/IpAddressFamily.cpp
+++ Modern Network/src/IpAddressFamily.cpp
@@ -2,9 +2,29 @@ module Net.IpAddress;
 import <utility>;
 import :IpAddressFamily;
 
+std::format_parse_context::iterator
+std::formatter<net::IpAddressFamily>::parse(std::format_parse_context& context)
+const noexcept
+{
+	return context.begin();
+}
+
 std::format_context::iterator
 std::formatter<net::IpAddressFamily>::format(const net::IpAddressFamily& fm, std::format_context& context)
 const noexcept
 {
-	return std::format_to(context.out(), "IP Address [{} / {}]", ip.GetFamily(), ip.GetAddress());
+	switch (fm)
+	{
+		case net::IpAddressFamily::IPv4:
+		{
+			return std::format_to(context.out(), "IPv4 Family");
+		}
+
+		case net::IpAddressFamily::IPv6:
+		{
+			return std::format_to(context.out(), "IPv6 Family");
+		}
+	}
+
+	return std::format_to(context.out(), "Unknown IP Address Family");
 }
