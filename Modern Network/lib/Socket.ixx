b2307e6f6ef00b775489c926943b87039beda54d
Iconer <yoyofa2@hotmail.com>
2023-10-31 20:19:39 +0900

    Also receivers could get a size with `std::span<std::byte>`

--- Modern Network/lib/Socket.ixx
+++ Modern Network/lib/Socket.ixx
@@ -107,6 +107,7 @@ export namespace net
 		SocketReceivingResult Receive(std::span<std::byte> memory, size_t size) const noexcept;
 		SocketReceivingResult Receive(_In_reads_bytes_(size)std::byte* const& memory, size_t size) const noexcept;
 		bool Receive(std::span<std::byte> memory, ReceivingErrorCodes& error_code) const noexcept;
+		bool Receive(std::span<std::byte> memory, size_t size, ReceivingErrorCodes& error_code) const noexcept;
 		bool Receive(_In_reads_bytes_(size)std::byte* const& memory, size_t size, ReceivingErrorCodes& error_code) const noexcept;
 
 		// Maybe asynchronous Receive
@@ -115,6 +116,7 @@ export namespace net
 		SocketReceivingResult Receive(_Maybenull_ IoContext& context, std::span<std::byte> memory, size_t size) const noexcept;
 		SocketReceivingResult Receive(_Maybenull_ IoContext& context, _In_reads_bytes_(size)std::byte* const& memory, size_t size) const noexcept;
 		bool Receive(_Maybenull_ IoContext& context, std::span<std::byte> memory, ReceivingErrorCodes& error_code) const noexcept;
+		bool Receive(_Maybenull_ IoContext& context, std::span<std::byte> memory, size_t size, ReceivingErrorCodes& error_code) const noexcept;
 		bool Receive(_Maybenull_ IoContext& context, _In_reads_bytes_(size)std::byte* const& memory, size_t size, ReceivingErrorCodes& error_code) const noexcept;
 
 		// Asynchronous Send & Receive
--- Modern Network/src/Socket_receivers.cpp
+++ Modern Network/src/Socket_receivers.cpp
@@ -1,6 +1,8 @@
 module;
 #pragma comment(lib, "Ws2_32.lib")
+#define NOMINMAX
 #include <WinSock2.h>
+#include <utility>
 
 module Net.Socket;
 
@@ -10,6 +12,22 @@ const noexcept
 {
 	if (int bytes = ::recv(myHandle
 		, reinterpret_cast<char*>(memory.data()), static_cast<int>(memory.size_bytes())
+		, 0); SOCKET_ERROR != bytes)
+	{
+		return bytes;
+	}
+	else
+	{
+		return unexpected(AcquireReceivingError());
+	}
+}
+
+net::SocketReceivingResult
+net::Socket::Receive(std::span<std::byte> memory, size_t size)
+const noexcept
+{
+	if (int bytes = ::recv(myHandle
+		, reinterpret_cast<char*>(memory.data()), static_cast<int>(std::min(memory.size_bytes(), size))
 		, 0); SOCKET_ERROR != bytes)
 	{
 		return bytes;
@@ -39,7 +57,7 @@ const noexcept
 bool
 net::Socket::Receive(std::span<std::byte> memory
 	, net::ReceivingErrorCodes& error_code)
-	const noexcept
+const noexcept
 {
 	return Receive(memory).and_then(
 		[](unsigned int&&) noexcept -> expected<bool, ReceivingErrorCodes> {
@@ -52,9 +70,23 @@ net::Socket::Receive(std::span<std::byte> memory
 }
 
 bool
+net::Socket::Receive(std::span<std::byte> memory, size_t size, ReceivingErrorCodes& error_code)
+const noexcept
+{
+	return Receive(memory, size).and_then(
+		[](unsigned int&&) noexcept -> expected<bool, ReceivingErrorCodes> {
+		return true;
+	}).or_else(
+		[&](net::ReceivingErrorCodes&& tr_error_code) noexcept -> expected<bool, ReceivingErrorCodes> {
+		error_code = std::move(tr_error_code);
+		return false;
+	}).value_or(false);
+}
+
+bool
 net::Socket::Receive(std::byte* const& memory, size_t size
 	, net::ReceivingErrorCodes& error_code)
-	const noexcept
+const noexcept
 {
 	return Receive(memory, size).and_then(
 		[](unsigned int&&) noexcept -> expected<bool, ReceivingErrorCodes> {
@@ -68,7 +100,7 @@ net::Socket::Receive(std::byte* const& memory, size_t size
 
 net::SocketReceivingResult
 net::Socket::Receive(IoContext& context, std::span<std::byte> memory)
-	const noexcept
+const noexcept
 {
 	::WSABUF buffer
 	{
@@ -101,8 +133,41 @@ net::Socket::Receive(IoContext& context, std::span<std::byte> memory)
 }
 
 net::SocketReceivingResult
+net::Socket::Receive(net::IoContext& context, std::span<std::byte> memory, size_t size) const noexcept
+{
+	::WSABUF buffer
+	{
+		.len = static_cast<::ULONG>(std::min(memory.size_bytes(), size)),
+		.buf = reinterpret_cast<char*>(const_cast<std::byte*>(memory.data())),
+	};
+
+	::DWORD bytes = 0;
+	::DWORD flags = 0;
+	if (0 == ::WSARecv(myHandle
+		, std::addressof(buffer), 1
+		, std::addressof(bytes)
+		, std::addressof(flags)
+		, reinterpret_cast<::LPWSAOVERLAPPED>(std::addressof(context))
+		, nullptr))
+	{
+		return bytes;
+	}
+	else
+	{
+		if (auto error = AcquireReceivingError(); error != ErrorCodes::PendedIoOperation)
+		{
+			return unexpected(std::move(error));
+		}
+		else
+		{
+			return 0U;
+		}
+	}
+}
+
+net::SocketReceivingResult
 net::Socket::Receive(IoContext& context, std::byte* const& memory, size_t size)
-	const noexcept
+const noexcept
 {
 	::WSABUF buffer
 	{
@@ -166,7 +231,7 @@ net::Socket::Receive(IoContext& context, std::byte* const& memory, size_t size
 
 net::Task<net::SocketReceivingResult>
 net::Socket::ReceiveAsync(net::IoContext& context, std::span<std::byte> memory)
-	const noexcept
+const noexcept
 {
 	if (SocketReceivingResult sent = Receive(context, memory); not sent)
 	{
@@ -193,8 +258,35 @@ net::Socket::ReceiveAsync(net::IoContext& context, std::span<std::byte> memory)
 }
 
 net::Task<net::SocketReceivingResult>
+net::Socket::ReceiveAsync(net::IoContext& context, std::span<std::byte> memory, size_t size) const noexcept
+{
+	if (SocketReceivingResult sent = Receive(context, memory, size); not sent)
+	{
+		co_return std::move(sent);
+	}
+
+	static ::DWORD flags = 0;
+	::DWORD transferred_bytes = 0;
+
+	::BOOL result = ::WSAGetOverlappedResult(myHandle
+		, reinterpret_cast<::LPWSAOVERLAPPED>(std::addressof(context))
+		, std::addressof(transferred_bytes)
+		, TRUE
+		, std::addressof(flags));
+
+	if (FALSE == result)
+	{
+		co_return net::unexpected(net::AcquireReceivingError());
+	}
+	else
+	{
+		co_return transferred_bytes;
+	}
+}
+
+net::Task<net::SocketReceivingResult>
 net::Socket::ReceiveAsync(net::IoContext& context, std::byte* const& memory, size_t size)
-	const noexcept
+const noexcept
 {
 	if (SocketReceivingResult sent = Receive(context, memory, size); not sent)
 	{
