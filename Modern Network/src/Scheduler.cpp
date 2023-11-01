fcc4ccd5c4b8143cc35933327a69df623dac191b
Iconer <yoyofa2@hotmail.com>
2023-11-01 20:10:36 +0900

    `Pause` and `Resume` of `coroutine::Schedule` is implemented

--- Modern Network/src/Scheduler.cpp
+++ Modern Network/src/Scheduler.cpp
@@ -1,6 +1,7 @@
 module;
 #include <thread>
 module Net.Scheduler;
+
 net::coroutine::Schedule::Schedule(net::coroutine::Schedule::handle_type handle, std::jthread&& worker)
 	: Handler(handle), coWorker(std::move(worker))
 noexcept
@@ -10,4 +11,18 @@ net::coroutine::Schedule::~Schedule()
 noexcept
 {
 	coWorker.request_stop();
+}
+
+std::suspend_always
+net::coroutine::Schedule::Pause()
+const noexcept
+{
+	return {};
+}
+
+void
+net::coroutine::Schedule::Resume()
+const noexcept
+{
+	myHandle.resume();
 }
