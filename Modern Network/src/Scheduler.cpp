d408ebfa2f6bf6eb5460c82d2c73f41099c3cb2a
Iconer <yoyofa2@hotmail.com>
2023-11-01 20:04:25 +0900

    Ctor and dtor of `coroutine::Schedule` is implemented

--- Modern Network/src/Scheduler.cpp
+++ Modern Network/src/Scheduler.cpp
@@ -1,2 +1,13 @@
 module;
+#include <thread>
 module Net.Scheduler;
+net::coroutine::Schedule::Schedule(net::coroutine::Schedule::handle_type handle, std::jthread&& worker)
+	: Handler(handle), coWorker(std::move(worker))
+noexcept
+{}
+
+net::coroutine::Schedule::~Schedule()
+noexcept
+{
+	coWorker.request_stop();
+}
