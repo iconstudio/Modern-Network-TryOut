db2d555509d546ed73f37139934241a39bad5d26
Iconer <yoyofa2@hotmail.com>
2023-11-24 18:31:39 +0900

    The `coroutine_handle` must be copied to the thread, not reference

--- Modern Network/src/ConcurrentAwaiter.cpp
+++ Modern Network/src/ConcurrentAwaiter.cpp
@@ -5,7 +5,7 @@ module Net.Coroutine.Awaiter.Concurrent;
 void
 net::coroutine::ConcurrentAwaiter::await_suspend(std::coroutine_handle<void> prev_handle)
 {
-	std::thread([&prev_handle] {
+	std::thread([prev_handle] {
 		prev_handle();
 	}).detach();
 }
