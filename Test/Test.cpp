34fa53d5532ad0bd710c102abcaf094b561712ae
Iconer <yoyofa2@hotmail.com>
2023-12-07 23:55:42 +0900

    Testing asynchronous accepting

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -29,6 +29,12 @@ size_t clientsNumber = 0;
 
 net::Socket lastClient = net::Socket::EmptySocket;
 net::io::Context acceptContext{};
+net::SocketResult acceptStatus{};
+volatile std::atomic_flag isClientReady{};
+
+net::Coroutine Accepter();
+net::Coroutine Worker();
+net::Coroutine Receiver();
 
 [[nodiscard]]
 inline std::string_view as_string(const std::span<const std::byte> buffer) noexcept
@@ -40,113 +46,6 @@ inline std::string_view as_string(const std::span<const std::byte> buffer) noexc
 inline std::string_view as_string(const std::span<const std::byte> buffer, const size_t size) noexcept
 {
 	return std::string_view{ reinterpret_cast<const char*>(buffer.data()), size };
-}
-
-net::Coroutine Accepter()
-{
-	std::println("Accepter started");
-
-	lastClient = net::Socket::Create(net::SocketType::Asynchronous, net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
-
-	auto reg_result = ioStation.Register(lastClient, 0);
-	if (reg_result)
-	{
-		std::println("The client is registered");
-	}
-	else
-	{
-		std::println("The client is not registered");
-	}
-
-	//while (true)
-	{
-		//auto acceptance = serverListener.Accept();
-
-		auto acceptance2 = serverListener.ReserveAccept(acceptContext, lastClient);
-		auto&& io_schedule = co_await ioStation.Schedule();
-
-		//if (acceptance.has_value())
-		if (acceptance2.has_value())
-		{
-			acceptContext.Clear();
-			//lastClient = std::move(acceptance2.value());
-		}
-		else
-		{
-			//std::println("The acceptance is failed due to '{}'", acceptance.error());
-			std::println("The acceptance is failed due to '{}'", acceptance2.error());
-			//break;
-		}
-	}
-
-	co_await net::coroutine::WaitForSeconds(1);
-	co_return;
-}
-
-net::Coroutine Worker()
-{
-	std::println("Worker is started");
-
-	auto&& io_schedule = co_await ioStation.Schedule();
-
-	while (true)
-	{
-		if (io_schedule->IsCancelled())
-		{
-			std::println("Worker has been cancelled");
-			break;
-		}
-
-		auto& io_event = co_await io_schedule->Start();
-
-		if (not io_event.isSucceed)
-		{
-			std::println("Worker has been failed");
-			break;
-		}
-	}
-}
-
-net::Coroutine Receiver()
-{
-	std::println("Receiver is started");
-
-	net::io::Context listen_context{};
-	listen_context.Clear();
-
-	std::byte recv_buffer[512]{};
-	size_t recv_size = 0;
-	//net::SocketClosingErrorCodes close_err;
-	//const bool closed = lastClient.Close(close_err);
-
-	std::span buffer{ recv_buffer };
-	std::span<std::byte> send_buf;
-
-	std::println("Receiving is started");
-
-	while (true)
-	{
-		auto recv_task = lastClient.MakeReceiveTask(listen_context, buffer);
-
-		auto recv = co_await recv_task;
-
-		if (recv.has_value())
-		{
-			recv_size = recv.value();
-			send_buf = buffer.subspan(0, recv_size);
-
-			std::println("Server received '{}' bytes: {}", recv_size, as_string(send_buf, recv_size));
-			listen_context.Clear();
-
-			auto sent = lastClient.Send(listen_context, send_buf, recv_size);
-			listen_context.Clear();
-		}
-		else
-		{
-			std::println("Server receives are failed due to '{}'", recv.error());
-			break;
-		}
-	}
 }
 
 int main()
@@ -197,12 +96,19 @@ int main()
 		std::println("The listener is opened!");
 	}
 
-	std::println("=========== Update ===========");
+	lastClient = net::Socket::Create(net::SocketType::Asynchronous, net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
 
-	auto runner = Accepter();
-	runner.Start();
-	auto worker = Receiver();
-	worker.StartAsync();
+	reg_result = ioStation.Register(lastClient, 2);
+	if (reg_result)
+	{
+		std::println("The client is registered");
+	}
+	else
+	{
+		std::println("The client is not registered");
+	}
+
+	std::println("=========== Update ===========");
 
 	std::vector<net::Coroutine> workers{};
 	for (size_t i = 0; i < 6; ++i)
@@ -210,6 +116,11 @@ int main()
 		auto& worker = workers.emplace_back(Worker());
 		worker.StartAsync();
 	}
+
+	auto runner = Accepter();
+	runner.Start();
+	auto worker = Receiver();
+	worker.StartAsync();
 
 	//std::println("Server is ended");
 
@@ -219,4 +130,102 @@ int main()
 	}
 
 	net::core::Annihilate();
+}
+
+net::Coroutine Accepter()
+{
+	std::println("Accepter started");
+
+	//while (true)
+	{
+		acceptStatus = serverListener.ReserveAccept(acceptContext, lastClient);
+	}
+
+	co_await net::coroutine::WaitForSeconds(1);
+	co_return;
+}
+
+net::Coroutine Worker()
+{
+	std::println("Worker is started");
+
+	auto&& io_schedule = co_await ioStation.Schedule();
+
+	while (true)
+	{
+		if (io_schedule->IsCancelled())
+		{
+			std::println("Worker has been cancelled");
+			break;
+		}
+
+		auto& io_event = co_await io_schedule->Start();
+
+		if (not io_event.isSucceed)
+		{
+			std::println("Worker has been failed");
+			break;
+		}
+
+		if (*io_event.ioContext == acceptContext)
+		{
+			if (acceptStatus.has_value())
+			{
+				acceptContext.Clear();
+				isClientReady.test_and_set();
+				isClientReady.notify_one();
+				//lastClient = std::move(acceptance2.value());
+			}
+			else
+			{
+				//std::println("The acceptance is failed due to '{}'", acceptance.error());
+				std::println("The acceptance is failed due to '{}'", acceptStatus.error());
+				//break;
+			}
+		}
+	}
+}
+
+net::Coroutine Receiver()
+{
+	while (not isClientReady.test());
+
+	std::println("Receiver is started");
+
+	net::io::Context listen_context{};
+	listen_context.Clear();
+
+	std::byte recv_buffer[512]{};
+	size_t recv_size = 0;
+	//net::SocketClosingErrorCodes close_err;
+	//const bool closed = lastClient.Close(close_err);
+
+	std::span buffer{ recv_buffer };
+	std::span<std::byte> send_buf;
+
+	std::println("Receiving is started");
+
+	while (true)
+	{
+		auto recv_task = lastClient.MakeReceiveTask(listen_context, buffer);
+
+		auto recv = co_await recv_task;
+
+		if (recv.has_value())
+		{
+			recv_size = recv.value();
+			send_buf = buffer.subspan(0, recv_size);
+
+			std::println("Server received '{}' bytes: {}", recv_size, as_string(send_buf, recv_size));
+			listen_context.Clear();
+
+			auto sent = lastClient.Send(listen_context, send_buf, recv_size);
+			listen_context.Clear();
+		}
+		else
+		{
+			std::println("Server receives are failed due to '{}'", recv.error());
+			break;
+		}
+	}
 }
