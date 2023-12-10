b3391a15c94335ee0736faf90eab80dc40935bf5
Iconer <yoyofa2@hotmail.com>
2023-12-10 23:00:13 +0900

    Chat test initiated

--- Test Chat Client/Test Chat Client.cpp
+++ Test Chat Client/Test Chat Client.cpp
@@ -1,7 +1,30 @@
 ﻿#pragma comment(lib, "Modern Network.lib")
-#include <iostream>
+#include <print>
+
+import Net;
+import Net.IpAddress;
+import Net.IpAddress.IPv4;
+import Net.Io.Context;
+import Net.Io.Station;
+import Net.Socket;
+import Net.SocketPool;
+import Net.Coroutine;
+import Net.Coroutine.Awaiter.Concurrent;
+
+net::Socket mySocket{};
+
+net::Coroutine Accepter();
+net::Coroutine Worker();
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	std::println("=========$ Server $=========");
+	std::println("=========== Init ===========");
+	net::core::Initialize();
+
+	std::println("=========== Awake ===========");
+	serverListener = net::Socket::Create(net::SocketType::Asynchronous, net::InternetProtocols::TCP, net::IpAddressFamily::IPv4);
+
+	std::println("=========== Start ===========");
+
 }
