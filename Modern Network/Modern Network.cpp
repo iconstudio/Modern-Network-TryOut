c3ede69d0bd40231bc93b9a7ae589a546cecd400
Iconer <yoyofa2@hotmail.com>
2023-12-16 14:09:30 +0900

    Unused assets are removed

--- Modern Network/Modern Network.cpp
+++ Modern Network/Modern Network.cpp
@@ -1,7 +1,1 @@
-﻿#include "pch.hpp"
-#include "stdafx.hpp"
-
-// TODO: 라이브러리 함수의 예제입니다.
-void fnModernNetwork()
-{
-}
+﻿
--- Modern Network/pch.hpp
+++
@@ -1,6 +0,0 @@
-﻿#ifndef PCH_H
-#define PCH_H
-
-#include "stdafx.hpp"
-
-#endif //PCH_H
